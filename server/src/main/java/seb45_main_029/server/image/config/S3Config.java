c59fa2b07d4fb03f67e4d3bf94de4a9cc7d0468c
uudean <swj4545@naver.com>
2023-10-13 23:15:59 +0900

    이미지 업로드 코드 리팩토링

--- server/src/main/java/seb45_main_029/server/image/config/S3Config.java
+++ server/src/main/java/seb45_main_029/server/image/config/S3Config.java
@@ -1,12 +1,8 @@
 package seb45_main_029.server.image.config;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
